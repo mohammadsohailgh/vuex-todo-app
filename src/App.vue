9fe8142fcc6382235e38322b55f50f8ea242c0f3
Mohammad Sohail <msohailcs@outlook.com>
2020-04-02 11:46:00 +0100

    components added and styled for 2 lists

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,37 @@
 <template>
   <div id="app">
+      <product-list-one v-bind:products="products"></product-list-one>
+      <product-list-two v-bind:products="products"></product-list-two>
     
   </div>
 </template>
 
 <script>
+import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
+
 export default {
-  
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo,
+  },
+  name: 'app',
   data () {
-    
+    return {
+      products: [
+                {name: 'Banana Skin', price: 20},
+                {name: 'Shiny Star', price: 40},
+                {name: 'Green Shells', price: 60},
+                {name: 'Red Shells', price: 80}
+            ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+body {
+  font-family: Ubuntu;
+  columns: #555;
 }
 </style>
